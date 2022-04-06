573351f41ad61101160a0c70226f9b4310ae401d
Vladimir Artemchenko <voartemchenko@gmail.com>
2022-04-06 17:30:00 +0600

    hot-fix-notab

--- notab/scripts/index.js
+++ notab/scripts/index.js
@@ -5,10 +5,10 @@ const missionDescription = document.querySelector('.mission__description');
 const contactForm = document.querySelector('.form');
 const formSubmitButton = document.querySelector('.form__submit-button');
 const formInput = contactForm.querySelectorAll('.form__input');
-const step = 0.4;
+const step = 0.2;
 const stepMission = 1;
 const start = 0;
-const finish = 93;
+const finish = cookiesBar.offsetHeight;
 let screenWidth;
 let width;
 let y = this.window.scrollY;
@@ -16,23 +16,24 @@ let mobile = false;
 let notShowedCookiesBar = true;
 let startOpacity = 1;
 const startAngle = 90;
-const startMarginPhone = 0;
+let startMarginPhone;
 const startMarginDescription = 0;
 let isThrottledShowCookiesBar = false;
 let isThrottledShowPhone = false;
 let notShowedPhone = true;
 
 function init() {
+
     screenWidth = document.documentElement.scrollWidth
     width = (screenWidth >= 1440) ? 1440 : screenWidth;
-    console.log(screenWidth);
-    console.log(width);
-    if (width > 767) {
-        phone.style.transform = 'translate(' + startMarginPhone + 'px,  0px) rotate(' + startAngle + 'deg)';
-    } else {
-        phone.style.transform = 'translate(' + startMarginPhone + 'px,  0px) rotate(' + 0 + 'deg)';
-    }
+    startMarginPhone = Number(getComputedStyle(phone, null).getPropertyValue("margin-left").slice(0, -2));
 
+
+    if (width > 767) {
+        phone.style.transform = 'translate(' + 0 + 'px,  0px) rotate(' + startAngle + 'deg)';
+    } else {
+        phone.style.transform = 'translate(' + 0 + 'px,  0px) rotate(' + 0 + 'deg)';
+    }
     missionDescription.style.transform = 'translate(' + startMarginDescription + 'px)';
     missionDescription.style.opacity = startOpacity;
 }
@@ -40,10 +41,8 @@ function init() {
 init();
 
 contactForm.addEventListener('submit', function (evt) {
-
     evt.preventDefault();
-    console.log("hmm");
-    formInput.forEach( function (item) {
+    formInput.forEach(function (item) {
         if (item.value) {
             item.classList.remove('form__input_type_empty');
         } else {
@@ -51,8 +50,6 @@ contactForm.addEventListener('submit', function (evt) {
         }
     });
 });
-
-
 
 window.addEventListener('resize', init);
 
@@ -68,22 +65,29 @@ function showCookiesBarOnMobile() {
 
 function showCookiesBar(start, finish) {
     if (isThrottledShowCookiesBar) {
+
         return
+
     } else {
+
         let x = start;
         cookiesBar.style.visibility = 'visible'
         let timer = setInterval(function () {
 
             if (x <= finish) {
+
                 cookiesBar.style.transform = 'translate( 0, ' + -x + 'px)';
                 x += step;
                 isThrottledShowCookiesBar = true;
+
             } else {
+
                 x = finish;
                 cookiesBar.style.transform = 'translate( 0, ' + -x + 'px)';
                 clearInterval(timer);
                 isThrottledShowCookiesBar = false;
                 notShowedCookiesBar = false;
+
             }
         }, 1)
     }
@@ -110,20 +114,23 @@ window.addEventListener('scroll', function () {
 function showPhone() {
 
     if (isThrottledShowPhone) {
+
         return
+
     } else {
 
-        let marginPhone = startMarginPhone;
+        let marginPhone = 0;
         let phoneAngle = startAngle;
         let marginDescription = startMarginDescription;
         let opacity = startOpacity;
-        const marginPhoneStep = (width / 2 - startMarginPhone - (phone.offsetWidth / 2)) / startAngle * step;
+        const marginPhoneStep = (width / 2 - startMarginPhone - phone.offsetWidth / 2) / startAngle * step;
         const marginDescriptionStep = (width - startMarginDescription) / startAngle * step;
         const opacityStep = startOpacity / startAngle * step;
 
         let timer = setInterval(function () {
 
             if (phoneAngle >= 0) {
+
                 phone.style.transform = 'translate(' + marginPhone + 'px,  0px) rotate(' + phoneAngle + 'deg)';
                 missionDescription.style.transform = 'translate(' + marginDescription + 'px)';
                 missionDescription.style.opacity = opacity;
@@ -132,16 +139,17 @@ function showPhone() {
                 marginPhone += marginPhoneStep;
                 opacity -= opacityStep;
                 isThrottledShowPhone = true;
+
             } else {
-                marginPhone = width / 2 - phone.offsetWidth / 2;
+
                 phoneAngle = 0;
-                marginDescription = width;
                 opacity = 0
                 phone.style.transform = 'translate(' + marginPhone + 'px,  0px) rotate(' + phoneAngle + 'deg)';
                 missionDescription.style.opacity = opacity;
                 clearInterval(timer);
                 isThrottledShowPhone = false;
                 notShowedPhone = false;
+
             }
         }, 1)
     }
