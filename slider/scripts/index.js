e6ee87f6dbc522a105cc9c0a555621ed9313df59
Vladimir Artemchenko <voartemchenko@gmail.com>
2022-04-06 21:23:34 +0600

    features priview

--- slider/scripts/index.js
+++ slider/scripts/index.js
@@ -10,7 +10,7 @@ let isThrottled = false;
 let x = 0;
 let isStart = true;
 
-// Preview-----------------------------------------------------------------------------------------
+// Preview доп-----------------------------------------------------------------------------------------
 
 const preview = document.querySelector('.popup_type_preview');
 const previewImage = document.querySelector('.popup__image');
@@ -54,6 +54,7 @@ function renderSlides() {
     buttons = document.querySelectorAll('.button');
 }
 
+
 renderSlides()
 
 function init() {
@@ -90,10 +91,7 @@ document.addEventListener('keyup', function(event) {
     }
   });
 
-<<<<<<< HEAD
-=======
 
->>>>>>> develop
 function rollSliderL() {
     if (isThrottled) {
         return
@@ -228,162 +226,3 @@ function reset(isStart) {
         images = document.querySelectorAll('.slider__slide');
     }
 }
-
-// проба тача
-
-
-
-let  sliderTrack = document.querySelector('.slider__slider-line'),
-  slides = document.querySelectorAll('.slider__slide'),
-  prev = document.querySelector('.control-button_prev'),
-  next = document.querySelector('.control-button_nxt'), 
-  slideWidth = width,
-  slideIndex = 0,
-  posInit = 0,
-  posX1 = 0,
-  posX2 = 0,
-  posY1 = 0,
-  posY2 = 0,
-  posFinal = 0,
-  isSwipe = false,
-  isScroll = false,
-  allowSwipe = true,
-  transition = true,
-  nextTrf = 0,
-  prevTrf = 0,
-  lastTrf = --slides.length * slideWidth,
-  posThreshold = slides[0].offsetWidth * 0.35,
-  trfRegExp = /([-0-9.]+(?=px))/,
-  swipeStartTime,
-  swipeEndTime,
-  
-  getEvent = function() {
-    return (event.type.search('touch') !== -1) ? event.touches[0] : event;
-  },
-
-  
-  swipeStart = function() {
-    let evt = getEvent();
-
-    if (allowSwipe) {
-
-      swipeStartTime = Date.now();
-
-      nextTrf = (slideIndex + 1) * -slideWidth;
-      prevTrf = (slideIndex - 1) * -slideWidth;
-
-      posInit = posX1 = evt.clientX;
-      posY1 = evt.clientY;
-
-      sliderTrack.style.transition = '';
-
-      document.addEventListener('touchmove', swipeAction);
-      document.addEventListener('mousemove', swipeAction);
-      document.addEventListener('touchend', swipeEnd);
-      document.addEventListener('mouseup', swipeEnd);
-    }
-  },
-  swipeAction = function() {
-
-    let evt = getEvent(),
-      style = sliderTrack.style.transform,
-      transform = +style.match(trfRegExp)[0];
-
-    posX2 = posX1 - evt.clientX;
-    posX1 = evt.clientX;
-
-    posY2 = posY1 - evt.clientY;
-    posY1 = evt.clientY;
-
-    if (!isSwipe && !isScroll) {
-      let posY = Math.abs(posY2);
-      if (posY > 7 || posX2 === 0) {
-        isScroll = true;
-        allowSwipe = false;
-      } else if (posY < 7) {
-        isSwipe = true;
-      }
-    }
-
-    if (isSwipe) {
-      if (slideIndex === 0) {
-        if (posInit < posX1) {
-          setTransform(transform, 0);
-          return;
-        } else {
-          allowSwipe = true;
-        }
-      }
-
-      // запрет ухода вправо на последнем слайде
-      if (slideIndex === --slides.length) {
-        if (posInit > posX1) {
-          setTransform(transform, lastTrf);
-          return;
-        } else {
-          allowSwipe = true;
-        }
-      }
-
-      if (posInit > posX1 && transform < nextTrf || posInit < posX1 && transform > prevTrf) {
-        reachEdge();
-        return;
-      }
-
-      sliderTrack.style.transform = `translate3d(${transform - posX2}px, 0px, 0px)`;
-    }
-
-  },
-  swipeEnd = function() {
-    posFinal = posInit - posX1;
-
-    isScroll = false;
-    isSwipe = false;
-
-    document.removeEventListener('touchmove', swipeAction);
-    document.removeEventListener('mousemove', swipeAction);
-    document.removeEventListener('touchend', swipeEnd);
-    document.removeEventListener('mouseup', swipeEnd);
-
-    if (allowSwipe) {
-      swipeEndTime = Date.now();
-      if (Math.abs(posFinal) > posThreshold || swipeEndTime - swipeStartTime < 300) {
-        if (posInit < posX1) {
-          slideIndex--;
-        } else if (posInit > posX1) {
-          slideIndex++;
-        }
-      }
-
-      if (posInit !== posX1) {
-        allowSwipe = false;
-        rollSliderL();
-      } else {
-        allowSwipe = true;
-      }
-
-    } else {
-      allowSwipe = true;
-    }
-
-  },
-  setTransform = function(transform, comapreTransform) {
-    if (transform >= comapreTransform) {
-      if (transform > comapreTransform) {
-        sliderTrack.style.transform = `translate3d(${comapreTransform}px, 0px, 0px)`;
-      }
-    }
-    allowSwipe = false;
-  },
-  reachEdge = function() {
-    transition = false;
-    swipeEnd();
-    allowSwipe = true;
-  };
-
-sliderTrack.style.transform = 'translate3d(0px, 0px, 0px)';
-
-
-sliderTrack.addEventListener('transitionend', () => allowSwipe = true);
-sliderTrack.addEventListener('touchstart', swipeStart);
-sliderTrack.addEventListener('mousedown', swipeStart);
